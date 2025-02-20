a2c29843935573da01e7151529682c6ec668da21
Ernesto Lanchares <elancha98@gmail.com>
2025-02-20 17:24:52 +0100

    Updated renderer API

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const c = @import("c.zig");
 const window = @import("render/window.zig");
 const vk = @import("render/vulkan.zig");
-const renderer = @import("render/renderer.zig");
+const Renderer = @import("render/renderer.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -11,7 +11,7 @@ pub fn main() !void {
         const w = try window.Window.create(800, 600, "explora");
         defer w.destroy();
 
-        var r = try renderer.Renderer.create(allocator, w);
+        var r = try Renderer.create(allocator, w);
         defer r.destroy();
 
         while (!w.shouldClose()) {
