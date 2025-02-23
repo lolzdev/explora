6c2e5edc1881191a7671ea7f4e7c4b2c22d91304
Lorenzo Torres <lorenzotorres@outlook.it>
2025-02-23 13:16:30 +0100

    Now GLFW is being built internally, no need to install the binaries.

--- build.zig
+++ build.zig
@@ -6,6 +6,55 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const glfw = b.addStaticLibrary(.{
+        .name = "glfw",
+        .target = target,
+        .optimize = optimize,
+    });
+    glfw.addCSourceFiles(.{ .files = &[_][]const u8{
+        "ext/glfw/src/cocoa_init.m",
+        "ext/glfw/src/cocoa_joystick.m",
+        "ext/glfw/src/cocoa_monitor.m",
+        "ext/glfw/src/cocoa_time.c",
+        "ext/glfw/src/cocoa_window.m",
+        "ext/glfw/src/context.c",
+        "ext/glfw/src/egl_context.c",
+        "ext/glfw/src/glx_context.c",
+        "ext/glfw/src/init.c",
+        "ext/glfw/src/input.c",
+        "ext/glfw/src/linux_joystick.c",
+        "ext/glfw/src/monitor.c",
+        "ext/glfw/src/nsgl_context.m",
+        "ext/glfw/src/null_init.c",
+        "ext/glfw/src/null_joystick.c",
+        "ext/glfw/src/null_monitor.c",
+        "ext/glfw/src/null_window.c",
+        "ext/glfw/src/osmesa_context.c",
+        "ext/glfw/src/platform.c",
+        "ext/glfw/src/posix_module.c",
+        "ext/glfw/src/posix_poll.c",
+        "ext/glfw/src/posix_thread.c",
+        "ext/glfw/src/posix_time.c",
+        "ext/glfw/src/vulkan.c",
+        "ext/glfw/src/wgl_context.c",
+        "ext/glfw/src/win32_init.c",
+        "ext/glfw/src/win32_joystick.c",
+        "ext/glfw/src/win32_module.c",
+        "ext/glfw/src/win32_monitor.c",
+        "ext/glfw/src/win32_thread.c",
+        "ext/glfw/src/win32_time.c",
+        "ext/glfw/src/win32_window.c",
+        "ext/glfw/src/window.c",
+        "ext/glfw/src/wl_init.c",
+        "ext/glfw/src/wl_monitor.c",
+        "ext/glfw/src/wl_window.c",
+        "ext/glfw/src/x11_init.c",
+        "ext/glfw/src/x11_monitor.c",
+        "ext/glfw/src/x11_window.c",
+        "ext/glfw/src/xkb_unicode.c",
+    }, .flags = &[_][]const u8{ "-D_GLFW_X11", "-Wall", "-Wextra" } });
+    glfw.linkLibC();
+
     const exe = b.addExecutable(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -14,7 +63,8 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.linkSystemLibrary("vulkan");
-    exe.linkSystemLibrary("glfw3");
+    exe.addIncludePath(b.path("ext/glfw/include"));
+    exe.linkLibrary(glfw);
     exe.linkLibC();
 
     compileAllShaders(b, exe);
