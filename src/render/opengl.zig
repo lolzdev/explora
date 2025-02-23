8b774a625daf941424543a33a46900b69fa0a8e4
h3ll <abdelrahman20191919@gmail.com>
2025-02-23 23:38:39 +0200

    uhh opengl

--- src/render/opengl.zig
+++ src/render/opengl.zig
@@ -2,3 +2,7 @@ const c = @cImport({
     @cDefine("GLAD_GL_IMPLEMENTATION", "1"); // Tells glad to implement the functions(believe me idk why)
     @cInclude("glad.h");
 });
+
+pub const Device = struct {
+    // device stuff
+};
