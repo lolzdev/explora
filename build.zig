19a447dbda611c45e12714fcdd7e4123c334ca07
Lorenzo Torres <lorenzotorres@outlook.it>
2025-02-23 12:40:17 +0100

    Added compatibility for Zig 0.13

--- build.zig
+++ build.zig
@@ -6,15 +6,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const exe = b.addExecutable(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-    });
-
-    const exe = b.addExecutable(.{
         .name = "explora",
-        .root_module = exe_mod,
     });
 
     exe.linkSystemLibrary("vulkan");
@@ -36,7 +32,9 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
