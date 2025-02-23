577175ac35a070c0ea24c62fb86d00c38b3c81bb
h3ll <abdelrahman20191919@gmail.com>
2025-02-23 22:28:50 +0200

    changed some stuff idk

--- build.zig
+++ build.zig
@@ -62,12 +62,16 @@ pub fn build(b: *std.Build) void {
         .name = "explora",
     });
 
+    //  If "opengl" was passed as an option, this statement will define USE_OPENGL,
+    //  which will be checked inside renderer.zig, it will use the opengl backend if that was defined,
+    //  else it won't thus the backend will be vulkan
+
     exe.linkSystemLibrary("vulkan");
     exe.addIncludePath(b.path("ext/glfw/include"));
-    exe.addIncludePath(b.path("src/render/"));
+    exe.addIncludePath(b.path("ext/gl/include"));
     exe.linkLibrary(glfw);
     exe.addCSourceFile(.{
-        .file = b.path("src/render/glad.c"),
+        .file = b.path("ext/gl/src/glad.c"),
         .flags = &[_][]const u8{"-Iinclude"},
     });
     exe.linkLibC();
